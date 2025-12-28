2d8291cf2a240ec4ba447fa728ab1d7772c24046
Kushagra Kumar <kumarkushagra777@gmail.com>
2025-12-28 21:11:50 +0530

    updating API to cache each entry

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,21 +1,42 @@
 from fastapi import FastAPI, HTTPException
 from image import ImageAnalyzer
+from image_cache import ImageCache
 import json
+import os
 import uvicorn
 
-app = FastAPI()
+from dotenv import load_dotenv
+load_dotenv()
+
+cache = ImageCache(supabase_url=os.getenv("SUPABASE_URL"),supabase_key=os.getenv("SUPABASE_KEY"))
 
 analyzer = ImageAnalyzer()
+app = FastAPI()
 
 @app.get("/analyze/{image_url:path}")
 async def analyze_image(image_url: str):
     try:
-        result_json = await analyzer.run(image_url)
-        return json.loads(result_json)
+        if cache.check(image_url):
+            cached_result = cache.get(image_url)
+            if cached_result is not None:
+                return cached_result
+
+        result_json_str = await analyzer.run(image_url)
+        result = json.loads(result_json_str)
+
+        if isinstance(result, dict) and "visual_dimensions" in result:
+            cache.update(image_url, result)
+
+        return result
 
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app",host="0.0.0.0",port=8000,reload=True,)
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True,
+    )
