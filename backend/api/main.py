c3d908db5a2ac2cc8637c116d78d7331ec6ab588
Kushagra Kumar <kumarkushagra777@gmail.com>
2026-01-03 04:36:01 +0530

    reduce database calls using walrus operator in main.py

--- backend/api/main.py
+++ backend/api/main.py
@@ -17,10 +17,9 @@ app = FastAPI()
 @app.get("/analyze/{image_url:path}")
 async def analyze_image(image_url: str):
     try:
-        if cache.check(image_url):
-            cached_result = cache.get(image_url)
-            if cached_result is not None:
-                return cached_result
+        if cached_result := cache.get(image_url):
+            # Using walrus operator, and reducing DB calls from 2 -> 1 (ONLY)
+            return cached_result
 
         result_json_str = await analyzer.run(image_url)
         result = json.loads(result_json_str)
